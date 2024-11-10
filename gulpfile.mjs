723c73b9d43345a97c40b78ac3facf694f03d982
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2024-11-10 11:11:44 -0600

    ADD: import devDependencies, compilarJS compilation function and watchFunciones function to watch changes in scss and js files

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,6 +1,6 @@
 // Importación de dependencias como modulo de sistema ECMAScript modules
 
-// sass
+// SASS
 import {src, dest, watch, series} from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
@@ -8,8 +8,14 @@ const sass = gulpSass(dartSass);
 import sourcemaps from 'gulp-sourcemaps';
 import postcss from 'gulp-postcss';
 
+// JS
+import terser from 'gulp-terser-js';
+import concat from 'gulp-concat';
+import rename from 'gulp-rename';
+
 const paths = {
-    scss: 'src/scss/**/*.scss'
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js'
 }
 
 function compilarSCSS() {
@@ -30,4 +36,34 @@ function compilarSCSS() {
     .pipe(dest('build/css'))
 }
 
-export default series(compilarSCSS);
+function compilarJS() {
+    return src(paths.js)
+    // sourcemaps
+    .pipe(sourcemaps.init())
+        // Concatenar
+        .pipe(concat('bundle.js'))
+        // //////
+        // Comprimir
+        .pipe(terser({
+            mangle: { toplevel: true }
+        }))
+        .on('error', function (error) {
+            this.emit('end')
+        })
+        // //////
+        // Renombrar
+        .pipe(rename({
+            suffix: '.min'
+        }))
+    .pipe(sourcemaps.write('.'))
+    // //////
+    .pipe(dest('build/js'))
+}
+
+// Watch
+function watchFunciones() {
+    watch(paths.scss, compilarSCSS)
+    watch(paths.js, compilarJS)
+}
+
+export default series(compilarSCSS, compilarJS, watchFunciones);
