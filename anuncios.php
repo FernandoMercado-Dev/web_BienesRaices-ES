2509f1e09efaa10deda0a800887c362579e4538d
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-01-16 13:26:15 -0600

    REFACTOR: replace static includes for header and footer with dynamic function from funciones.php

--- anuncios.php
+++ anuncios.php
@@ -1,4 +1,7 @@
-<?php include 'includes/templates/header.php' ?>
+<?php 
+    require 'includes/funciones.php';
+    incluirTemplate('header');
+?>
 
     <main class="contenedor seccion">
         <h2>Casas y Depas en Venta</h2>
@@ -218,4 +221,7 @@
         <!-- .contenedor-anuncios -->
     </main>
 
-<?php include 'includes/templates/footer.php' ?>
+
+<?php 
+    incluirTemplate('footer');
+?>
