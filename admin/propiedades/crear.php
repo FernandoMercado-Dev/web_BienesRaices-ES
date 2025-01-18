7a45a1652156c0d45bae3851e27e9c072192e246
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-01-17 18:01:51 -0600

    CREATE: form for creating a new house ad in admin/propiedades/crear.php

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,49 @@
         <h1>Crear</h1>
 
         <a href="/admin/" class="boton boton-verde">Volver</a>
+
+        <from class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio Propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+                
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="estacionamietno">Estacionamietno:</label>
+                <input type="number" id="estacionamietno" placeholder="Ej: 3" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select>
+                    <option value="" selected disabled>-- Selecciona --</option>
+                    <option value="1">Fernando</option>
+                    <option value="2">Karen</option>
+                </select>
+
+                <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+            </fieldset>
+        </from>
     </main>
 
 
