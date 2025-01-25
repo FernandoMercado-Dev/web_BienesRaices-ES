fc66306eaf6e94de823b2c7f847247533d2c4b52
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-01-24 18:45:12 -0600

    CREATE: login.php with autentication form for users

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php 
+    require 'includes/funciones.php';
+    incluirTemplate('header');
+?>
+
+    <main class="contenedor seccion contenido-centrado">
+        <h1>Iniciar Sesión</h1>
+
+        <form action="" class="formulario">
+            <fieldset>
+                <legend>Email y Password</legend>
+
+                <label for="email">E-mail</label>
+                <input type="email" placeholder="Tu E-mail" id="email">
+
+                <label for="password">Password</label>
+                <input type="password" placeholder="Tu Password" id="password">
+
+            </fieldset>
+
+            <input type="submit" value="Iniciar Sesión" class="boton boton-verde">
+        </form>
+    </main>
+
+
+<?php 
+    incluirTemplate('footer');
+?>
