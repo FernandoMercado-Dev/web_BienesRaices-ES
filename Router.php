e11ce8161ba3f9db0de7d71b2c8d6b9e130dccf6
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-02-25 19:20:35 -0600

    ADD: methods in Router.php for handling path info and request method, fetching routes from public/index.php

--- Router.php
+++ Router.php
@@ -3,8 +3,28 @@
 namespace MVC;
 
 class Router {
-    public function __construct()
-    {
-        
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    // Llenar el arreglo de rutasGET con las url y funciones asociadas
+    public function get($url, $fn) {
+        $this->rutasGET[$url] = $fn;
+    }
+    
+    public function comprobarRutas() {
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if($metodo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if($fn) {
+            // La url existe y hay una funcion asociada
+            call_user_func($fn, $this);
+        } else {
+            echo "Página no encontrada";
+        }
     }
 }
