6f14c1678b523bd4b66fee209323330f02dfae86
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-02-26 19:06:40 -0600

    ADD: back button to return to admin page in views/propiedades/crear.php

--- views/propiedades/crear.php
+++ views/propiedades/crear.php
@@ -6,6 +6,8 @@
                 <?php echo $error ?>
             </div>
     <?php endforeach; ?>
+
+    <a href="/admin" class="boton boton-verde">Volver</a>
     
     <form class="formulario" method="POST" enctype="multipart/form-data">
         <?php include __DIR__ . '/formulario.php' ?>
