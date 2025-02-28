8d5ca877109be744d2c88be34748456f6522fa27
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-02-28 17:29:27 -0600

    CREATE: controllers/PaginasControllers.php for handle all methods of routes in public/index.php

---
+++ controllers/PaginasController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Controllers;
+
+class PaginasController {
+    public static function index() {
+        echo "Hola ;)";
+    }
+
+    public static function nosotros() {
+        echo "Hola ;)";
+    }
+
+    public static function propiedades() {
+        echo "Hola ;)";
+    }
+
+    public static function propiedad() {
+        echo "Hola ;)";
+    }
+
+    public static function blog() {
+        echo "Hola ;)";
+    }
+
+    public static function entrada() {
+        echo "Hola ;)";
+    }
+
+    public static function contacto() {
+        echo "Hola ;)";
+    }
+}
