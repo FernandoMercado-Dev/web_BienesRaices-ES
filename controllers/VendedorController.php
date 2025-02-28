499af323a14d17e1f12f0eaf81cd09006f5a89b8
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-02-28 13:22:01 -0600

    ADD: error messages, form data persistence, and dynamic variables in crear method of VendedorController.php

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -3,10 +3,19 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Vendedor;
 
 class VendedorController {
     public static function crear(Router $router) {
-        echo "crear Vendedor";
+
+        $errores = Vendedor::getErrores();
+
+        $vendedor = new Vendedor;
+
+        $router->render('/vendedores/crear', [
+            'errores' => $errores,
+            'vendedor' => $vendedor
+        ]);
     }
 
     public static function actualizar() {
