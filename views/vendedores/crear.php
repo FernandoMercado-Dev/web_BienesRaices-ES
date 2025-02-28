5fe088bfac5beabeaf2cb8abab24366046bc0360
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-02-28 16:30:32 -0600

    UPDATE: change action form path in views/vendedores/crear.php

--- views/vendedores/crear.php
+++ views/vendedores/crear.php
@@ -1,7 +1,7 @@
 <main class="contenedor seccion">
     <h1>Registrar Vendedor(a)</h1>
 
-    <a href="/admin/" class="boton boton-verde">Volver</a>
+    <a href="/admin" class="boton boton-verde">Volver</a>
 
     <?php foreach($errores as $error) :  ?>
         <div class="alerta error">
@@ -9,7 +9,7 @@
         </div>
     <?php endforeach; ?>
 
-    <form class="formulario" action="/admin/vendedores/crear.php" method="POST">
+    <form class="formulario" action="/vendedores/crear" method="POST">
         <?php include 'formulario.php' ?>
 
         <input type="submit" value="Registrar Vendedor" class="boton boton-verde">
