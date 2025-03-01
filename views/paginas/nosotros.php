4096372066adb760318597ad67d944d06ee38e79
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-02-28 19:01:46 -0600

    CREATE: views/paginas/iconos.php and move iconos-nosotros section from paginas/nosotros.php; include it in paginas/nosotros.php and paginas/index.php

--- views/paginas/nosotros.php
+++ views/paginas/nosotros.php
@@ -25,21 +25,5 @@
 <section class="contenedor seccion">
     <h1>Más Sobre Nosotros</h1>
 
-    <div class="iconos-nosotros">
-        <div class="icono">
-            <img src="build/img/icono1.svg" alt="Icono seguridad" loading="lazy">
-            <h3>Seguridad</h3>
-            <p>Voluptatem adipisci error sequi magnam vel atque deleniti tempore! Corporis, illo, aliquam perferendis debitis hic, corrupti fugit porro optio provident animi pariatur.</p>
-        </div>
-        <div class="icono">
-            <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
-            <h3>Precio</h3>
-            <p>Voluptatem adipisci error sequi magnam vel atque deleniti tempore! Corporis, illo, aliquam perferendis debitis hic, corrupti fugit porro optio provident animi pariatur.</p>
-        </div>
-        <div class="icono">
-            <img src="build/img/icono3.svg" alt="Icono Tiempo" loading="lazy">
-            <h3>A Tiempo</h3>
-            <p>Voluptatem adipisci error sequi magnam vel atque deleniti tempore! Corporis, illo, aliquam perferendis debitis hic, corrupti fugit porro optio provident animi pariatur.</p>
-        </div>
-    </div>
+    <?php include 'iconos.php'; ?>
 </section>
