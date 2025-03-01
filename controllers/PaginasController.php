b38f911529d87b8f5dbae6021912e8734944f3ec
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-03-01 17:56:18 -0600

    ADD: configure phpmailer with Mailtrap for smtp email handling in contacto method of PaginasController.php

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use MVC\Router;
 use Model\Propiedad;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class PaginasController {
     public static function index(Router $router) {
@@ -54,6 +55,43 @@ class PaginasController {
     }
 
     public static function contacto(Router $router) {
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+            // Crear una instancia de phpmailer
+            $mail = new PHPMailer();
+
+            // Configurar SMTP (protocolo que se usa para el envio de email como si fuera http)
+            $mail->isSMTP();
+            $mail->Host = 'sandbox.smtp.mailtrap.io';
+            $mail->SMTPAuth = true;
+            $mail->Username = '3c6fcb8686e2ed';
+            $mail->Password = '434a22dc248ff8';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = '2525';
+
+            // Configurar el contenido del email
+            $mail->setFrom('admin@bienesraices.com');
+            $mail->addAddress('admin@bienesraices.com', 'BienesRaices.com');
+            $mail->Subject = 'Tienes un nuevo mensaje';
+
+            // Habilitar HTML
+            $mail->isHTML(true);
+            $mail->CharSet = 'UTF-8';
+
+            // Definir contenido
+            $contenido = '<html> <p>Tienes un nuevo mensaje</p> </html>';
+
+            $mail->Body = $contenido;
+            $mail->AltBody = 'Esto es texto alternativo sin HTML';
+
+            // Enviar el email
+            if($mail->send()) {
+                echo "Mensaje enviado correctamente";
+            } else {
+                echo "El mensaje no se pudo enviar ...";
+            }
+        }
         
         $router->render('paginas/contacto', [
 
