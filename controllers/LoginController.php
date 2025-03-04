758fedbf170db1cf36811a4ecc8a836d6d6a34d5
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-03-03 18:46:00 -0600

    CREATE: controllers/LoginController.php with login and logout method

---
+++ controllers/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+use Model\Admin;
+
+class LoginController{
+
+    public static function login(Router $router) {
+        
+        
+    }
+
+    public static function logout() {
+        echo "desde logout";
+    }
+}
