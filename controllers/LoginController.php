57b7d6db7d27deba2fd275a1b3b9bf39a701636f
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-03-03 18:57:48 -0600

    CHANGE: relocate login.php to views/auth/ folder and ADD: render auth/login.php page from login method in LoginController.php

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -8,8 +8,16 @@ use Model\Admin;
 class LoginController{
 
     public static function login(Router $router) {
+
+        $errores = [];
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
         
-        
+        $router->render('auth/login', [
+            'errores' => $errores
+        ]);
     }
 
     public static function logout() {
