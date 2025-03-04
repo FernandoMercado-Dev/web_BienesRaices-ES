92119d7c1471bbffacbc109f0ca65b585a90fbd2
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-03-04 14:11:27 -0600

    UPDATE: change path of logout button in views/layout.php to redirect to /logout, using logout method from LoginController.php that handles session termination and redirects to the main page

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -45,6 +45,11 @@ class LoginController {
     }
 
     public static function logout() {
-        echo "desde logout";
+        
+        session_start();
+        
+        $_SESSION = [];
+
+        header('Location: /');
     }
 }
