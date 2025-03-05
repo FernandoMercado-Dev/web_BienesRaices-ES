94e77c0e254317e9c70c17209303087322322c34
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-03-04 19:31:11 -0600

    ADD: database connection variables in includes/config/database.php, sourcing values from config.php, and adding config.php to .gitignore for security

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,11 @@
 <?php
 
+require_once __DIR__ . '/config.php';
+
 function conectarDB() : mysqli {
-    $db = new mysqli('localhost', 'root', 'root', 'bienesraices_crud');
+    global $DB_host, $DB_user, $DB_password, $DB_database;
+
+    $db = new mysqli($DB_host, $DB_user, $DB_password, $DB_database);
 
     if(!$db) {
         echo "Error no se pudo conectar";
