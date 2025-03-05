e347020509e79cb0077f88cba3c121f0deed3d0f
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-03-04 19:41:28 -0600

    UPDATE: change button path in views/paginas/index.php to direct to views/paginas/contacto.php and views/paginas/entrada.php

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -19,7 +19,7 @@
 <section class="imagen-contacto">
     <h2>Encuentra la casa de tus sueños</h2>
     <p>Llena el formulario de contacto y un asesor se pondrá en contacto contigo a la brevedad</p>
-    <a href="contacto.php" class="boton-amarillo">Contáctanos</a>
+    <a href="contacto" class="boton-amarillo">Contáctanos</a>
 </section>
 
 <div class="contenedor seccion seccion-inferior">
@@ -36,7 +36,7 @@
             </div>
 
             <div class="texto-entrada">
-                <a href="entrada.php">
+                <a href="entrada">
                     <h4>Terraza en el techo de tu casa</h4>
                     <p class="informacion-meta">Escrito el: <span>20/10/2024</span> por: <span>Admin</span></p>
 
@@ -59,7 +59,7 @@
             </div>
 
             <div class="texto-entrada">
-                <a href="entrada.php">
+                <a href="entrada">
                     <h4>Guía para la decoración de tu hogar</h4>
                     <p class="informacion-meta">Escrito el: <span>20/10/2024</span> por: <span>Admin</span></p>
 
